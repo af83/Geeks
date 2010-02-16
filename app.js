d6155f81c17fac83394390a2877711c1ba244506
Arnaud Berthomier <arnaud@cyprio.net>
2010-02-16 23:31:42 +0100

    Now fully HAML-ized. Enforce programming style.

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
 // My little node.js stuff.
-var posix   = require('posix'),
+var posix   = require("posix"),
     events  = require("events"),
     emitter = new events.EventEmitter()
 
 // Load Express from Visionmedia
-require.paths.unshift('vendor/express/lib');
-require('express');
-require('express/plugins');
+require.paths.unshift("vendor/express/lib");
+require("express");
+require("express/plugins");
 
 configure(function() {
     use(MethodOverride);
     use(ContentLength);
-    set('root', __dirname);
+    use(CommonLogger);
+    set("root", __dirname);                  // Root dir
+    set("geeks", set("root") + "/geeks");    // Geeks JSON store dir
 });
 
 // Home page
 get('/', function() {
-    self = this;
-    this.contentType('text');
-    posix.readdir(set('root')+"/geeks")
-    .addCallback(function(files_name) {
+    var self = this;
+    this.contentType("html");
+
+    posix.readdir(set("geeks"))
+    .addCallback(function(file_list) {
             var acc = [];
             emitter.addListener("oneMoreFile",
-                function(geek){
+                function(geek) {
                     acc.push(geek);
-                    if( acc.length  == files_name.length) {
-                        self.render('home.haml.html', { locals: { geeks: acc}})
+                    if (acc.length  == file_list.length) {
+                        self.render("home.haml.html", { locals: { geeks: acc } });
                     }
-            })
-            for(file_name in files_name) {
-                posix.cat(set('root')+"/geeks/"+files_name[file_name])
+                }
+            );
+            for (file_name in file_list) {
+                posix.cat(set("geeks") + "/" + file_list[file_name])
                 .addCallback(function(data) {
-                    emitter.emit("oneMoreFile", JSON.parse(data))
+                    emitter.emit("oneMoreFile", JSON.parse(data));
                 });
             }
         }
     )
-
-    var geeks = [{name: '1'}, {name: '22'}]
+    //var geeks = [{name: '1'}, {name: '22'}]
 });
 
 /**
@@ -46,107 +49,104 @@ get('/', function() {
  * Submiting a changed geek.
  * @todo timeout
  */
-get('/events', function() {
+get("/events", function() {
     var self = this;
-    this.contentType('text');
+    this.contentType("text");
+
     // watching geeks dir
-    posix.readdir(set('root')+"/geeks")
+    posix.readdir(set("geeks"))
     .addCallback(function(files_name) {
         // use for watching a geek file and emit it on change as json.
-        var watchFile = function(file_name){
+        var watchFile = function(file_name) {
             process.watchFile(file_name,
-                function(current, previous){
+                function(current, previous) {
                     posix.cat(file_name)
-                    .addCallback(function(data){
+                    .addCallback(function(data) {
                        self.halt(200, data);
                     });
                 }
             )
         }
         // listenning at each geek change
-        for(file_name in files_name){
-            watchFile(set('root')+"/geeks/"+files_name[file_name]);
+        for (file_name in files_name) {
+            watchFile(set("geeks") + "/" + files_name[file_name]);
         }
     })
-    emitter.addListener("newGeeks", function(file_name){
+    emitter.addListener("newGeeks", function(file_name) {
         posix.cat(file_name)
-        .addCallback(function(data){
+        .addCallback(function(data) {
             self.halt(200, data);
         })
     });
-
 });
 
 /**
  * listing saved geeks.
  */
-get('/test', function() {
+get("/test", function() {
     var self = this;
-    this.contentType('text');
+    this.contentType("text");
+
     // agregate all geeks as an array
-    posix.readdir(set('root')+"/geeks")
+    posix.readdir(set("geeks"))
     .addCallback(function(files_name) {
             var acc = [];
             emitter.addListener("oneMoreFile",
-                function(geek){
+                function(geek) {
                     acc.push(geek);
-                    if( acc.length  == files_name.length) {
-                        self.render('test.haml.html', { locals: { geeks: acc}})
+                    if (acc.length == files_name.length) {
+                        self.render("test.haml.html", { locals: { geeks: acc } });
                     }
-            })
-            for(file_name in files_name) {
-                posix.cat(set('root')+"/geeks/"+files_name[file_name])
+            });
+            for (file_name in files_name) {
+                posix.cat(set("geeks") + "/" + files_name[file_name])
                 .addCallback(function(data) {
-                    emitter.emit("oneMoreFile", JSON.parse(data))
+                    emitter.emit("oneMoreFile", JSON.parse(data));
                 });
             }
         }
     )
 });
 
-
 /*
 * Write a geek to the file system.
 */
-post('/geek/new', function() {
+post("/geek/new", function() {
     var self = this;
-    debug(this.param('name'))
-    var file_name = set('root')+"/geeks/"+this.param('name')+".json";
+    debug(this.param("name"));
+    var file_name = set("geeks") + "/" + this.param("name") + ".json";
     var fd = posix.open(file_name, process.O_CREAT | process.O_WRONLY, 0666)
-    .addCallback(function (fd){
+    .addCallback(function (fd) {
         posix.write(fd, JSON.stringify(self.params.post)) // tojson
-        .addCallback(function(bytes){
+        .addCallback(function(bytes) {
             posix.close(fd)
-            .addCallback(function(){
-                debug(file_name)
+            .addCallback(function() {
+                debug(file_name);
                 emitter.emit("newGeeks", file_name);
                 self.halt(200, "done");
-            }).addErrback(function(){
+            }).addErrback(function() {
                 self.halt(400, "failed");
             });
-        }).addErrback(function(){
+        }).addErrback(function() {
                 self.halt(400, "failed");
          })
-        .addErrback(function(){
+        .addErrback(function() {
             self.halt(400, "failed");
         });
     });
 });
 
-
 /**
  * form to create a geek to the file system
  * @todo gruik, returning an hard file, haven't debug this.render('new_geek.mustache.html') for now.
  */
-get('/geek/new', function(){
-    //this.sendfile(set('root')+'/views/new_geek.haml.html');
-    this.render("new_geek.haml.html", {layout: false});
+get("/geek/new", function(){
+    this.render("new_geek.haml.html", { layout: false });
 });
 
-// Render static files
+// Render all static files
 get('/*', function(file) {
   this.sendfile(set('root') + '/public/' + file)
 });
-
 
 run();
