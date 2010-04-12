1075cf4bf338c365a1ba37c124924bc85051639c
zyll <zyll@zyll.faberlan.com>
2010-04-13 00:19:29 +0200

    Initial irc stubs spec

---
+++ barIrc.js
@@ -0,0 +1,52 @@
+GLOBAL.DEBUG = true;
+var sys = require('sys')
+
+// My little node.js stuff.
+var events  = require("events")
+
+// Express
+require.paths.unshift("vendor/express/lib");
+require("express");
+require("express/plugins");
+var utils = require('express/utils'),
+    kiwi  = require('kiwi');
+
+
+configure(function() {
+  kiwi.seed('haml');
+  use(MethodOverride);
+  use(ContentLength);
+  use(CommonLogger);
+});
+
+/**
+ * Home page
+ * Who am I.
+ */
+get('/', function() {
+});
+
+/**
+ * Which params i need to start listenning.
+ * @todo app.js config and call this url.
+ */
+get('/carte', function() {
+    halt(200, JSON.stringify( { 'params' : ['server', 'chanel', 'nick'],
+                             'name' : 'irc',
+                             'version' : 'alpha (as in GFI)'
+    }))
+});
+
+/**
+ * @param geek
+ * @param url
+ */
+post('/order', function() {
+    // check params
+    // contact service and watch.
+    // branch callback on /
+})
+
+
+
+run();
