46e7314c6610ef83dce8e7c2a9d1aa2fd3ddfd4c
François de Metz <fdemetz@af83.com>
2011-01-14 12:25:30 +0100

    add authority and domain for authentication.
    Signed-off-by: François de Metz <fdemetz@af83.com>
    Signed-off-by: Virtuo <pierre@ruyssen.fr>

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ var http = require('http')
 
   , connect = require('connect')
   , connect_form = require('connect-form')
-  , sessions = require('cookie-sessions')  
+  , sessions = require('cookie-sessions')
   , bserver = require('nodetk/browser/server')
   , rest_server = require('rest-mongo/http_rest/server')
   , oauth2_client = require('oauth2_client')
@@ -27,7 +27,7 @@ var http = require('http')
   , geeks_app = require('./geeks_app')
   , geeks_events = require('./geeks_events')
   , ms_templates = require('./ms_templates')
-  , RFactory = require('./model').RFactory  
+  , RFactory = require('./model').RFactory
   , schema = require('./schema').schema
   ;
 
@@ -35,14 +35,15 @@ var http = require('http')
 var oauth2_client_options = {
   // To get info from access_token and set them in session
   treat_access_token: function(access_token, req, res, callback, fallback) {
-    var params = {oauth_token: access_token};
-    // TODO: put this in config file!
-    web.GET(config.oauth2_client.server_base_url + '/auth', params, 
+    var params = {oauth_token: access_token,
+                  authority: config.oauth2_client.authority,
+                  domain: config.oauth2_client.domain};
+    web.GET(config.oauth2_client.server_base_url + '/auth', params,
     function(status_code, headers, body) {
-      if(status_code != 200) 
+      if(status_code != 200)
         return fallback("Bad answer from AuthServer: "+status_code+" "+body);
       var info = JSON.parse(body);
-      req.session.user_email = info.email || info.name;
+      req.session.user_email = info.userid || info.name;
       callback();
     });
   }
@@ -75,7 +76,7 @@ var serve = function(port, callback, websocket_port) {
   if(port != websocket_port) {
     ws_server = http.createServer();
     ws_server.listen(websocket_port, function() {
-      console.log('WS server listning...');    
+      console.log('WS server listening...');
     });
   }
   ms_templates.generate_refresh_templates(function() {
@@ -87,8 +88,8 @@ var serve = function(port, callback, websocket_port) {
 
 if(process.argv[1] == __filename) {
   var port = config.server.port;
-  serve(port, function() {
-    console.log('Geeks listning on http://localhost:'+port);
+  serve(port, function(err) {
+    if (err) throw err;
+    console.log('Geeks listening on http://localhost:'+port);
   }, config.server.websocket_port);
 }
-
